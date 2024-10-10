387bd7ce484c3bd21cb23dbca1d3a046ba4822cf
Abdul Mannan <abdulmannan_techoutsource@outlook.com>
2024-10-10 08:29:36 +0500

    chore(refactor): update routing logic for selecting organization and building
    Refactored the code to handle automatic navigation between /home and /home/:organizationId. Adjusted
    the conditional logic to redirect users to /home/:organizationId if a valid organizationId exists in
    local storage. Removed redundant code for better readability and performance.

--- src/pages/home/index.svelte
+++ src/pages/home/index.svelte
@@ -5,18 +5,21 @@
   import { multiSearchOr } from "$helpers/search.js";
   import { buildImageUrl } from "$helpers/formatters.js";
   import { guid_to_base64 } from "$helpers/guid.js";
+
   import SolidSearchIcon from "$icons/SolidSearchIcon.svelte";
   import OutlineArrowSmallRightIcon from "$icons/OutlineArrowSmallRightIcon.svelte";
 
-  let filteredbuildings = [];
+  let filteredOrganizations = [];
+
   let lastUid = localStorage.getItem("uid");
   let lastOrganizationId = localStorage.getItem("organizationId");
   let lastBuildingId = localStorage.getItem("buildingId");
+
   let showList = true;
 
   if (lastUid === $user.userData.uid && lastOrganizationId !== null && lastBuildingId !== null) {
     showList = false;
-    $goto("/", {
+    $goto("/home/:organizationId/:buildingId", {
       organizationId: guid_to_base64(lastOrganizationId),
       buildingId: guid_to_base64(lastBuildingId),
     });
@@ -44,11 +47,11 @@
 
   function updateSearch(val) {
     if (!$user.userData || !$user.userData.extendedData || !$user.userData.extendedData.organizations) {
-      filteredbuildings = [];
+      filteredOrganizations = [];
       return;
     }
     if (val === "") {
-      filteredbuildings = $user.userData.extendedData.organizations;
+      filteredOrganizations = $user.userData.extendedData.organizations;
       return;
     }
 
@@ -58,7 +61,7 @@
       return;
     }
 
-    filteredbuildings = $user.userData.extendedData.organizations.filter((item) => {
+    filteredOrganizations = $user.userData.extendedData.organizations.filter((item) => {
       return multiSearchOr(item.name, searchTerms);
     });
   }
@@ -67,7 +70,7 @@
     if (organizationId === null) {
       return;
     }
-    $goto("/select_building", { organizationId: guid_to_base64(organizationId) });
+    $goto("/home/:organizationId", { organizationId: guid_to_base64(organizationId) });
   }
 
   // If user is only assigned to one organization, navigate to the next page automatically.
@@ -78,13 +81,13 @@
     $user.userData.extendedData.organizations &&
     $user.userData.extendedData.organizations.length === 1
   ) {
-    handleClick($user.userData.extendedData.organizations[0].id.buildingId);
+    handleClick($user.userData.extendedData.organizations[0].id);
   }
+
   if (showList) {
     updateSearch("");
   }
 </script>
-
 
 {#if showList && $user.userData && $user.userData.extendedData && $user.userData.extendedData.organizations && $user.userData.extendedData.organizations.length > 1}
   <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-background">
@@ -95,7 +98,7 @@
       </div>
 
       <div class="text-blacktext" style="font-size: 2rem;">
-        {$t("main.selectBuilding")}
+        {$t("main.selectOrganization")}
       </div>
       <div class="mt-4 relative w-full border ring-1 ring-black ring-opacity-5">
         <SolidSearchIcon className="pointer-events-none absolute top-3 left-4 h-8 w-8 text-gray-400" />
@@ -105,22 +108,20 @@
           on:keydown={handleSearchKeydown}
           on:input={handleSearchInput}
           class="h-12 w-full border-0 bg-transparent pl-[4rem] pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 text-2xl mb-1"
-          placeholder={$t("main.searchBuildingsName")}
+          placeholder={$t("main.searchOrganizationsName")}
         />
       </div>
       <div class="h-[17rem] overflow-y-scroll bg-white shadow ring-1 ring-black ring-opacity-5 rounded-b-md">
         <ul class="divide-y divide-gray-200">
-          {#each filteredbuildings as building, i (building.id)}
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-            <li class="cursor-pointer" on:click={() => handleClick(building.id)}>
+          {#each filteredOrganizations as organization, i (organization.id)}
+            <li class="cursor-pointer" on:click={() => handleClick(organization.id)}>
               <div class="block hover:bg-gray-100" class:bg-gray-50={i % 2 == 0}>
                 <div class="flex items-center pr-4 py-2 sm:pr-6">
                   <div class="flex min-w-0 flex-1 basis-full md:basis-2/5 items-center">
                     <div class="min-w-0 px-4">
                       <div>
                         <p class="truncate text-2xl font-medium">
-                          {building.name}
+                          {organization.name}
                         </p>
                       </div>
                     </div>
@@ -129,7 +130,7 @@
                     <div class="min-w-0 px-4">
                       <div>
                         <p class="flex items-center text-2xl text-gray-500">
-                          <img class="max-h-6" alt={building.name} src={buildImageUrl(building.logoImageUrl)} />
+                          <img class="max-h-6" alt={organization.name} src={buildImageUrl(organization.logoImageUrl)} />
                         </p>
                       </div>
                     </div>
